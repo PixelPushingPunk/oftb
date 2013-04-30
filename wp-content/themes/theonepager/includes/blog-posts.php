62d8616f0a6767c9f917bc95f25aafc51d82788b
Ar <hola.juapa@gmail.com>
2013-04-30 17:23:44 +0100

    twitter

--- wp-content/themes/theonepager/includes/blog-posts.php
+++ wp-content/themes/theonepager/includes/blog-posts.php
@@ -64,19 +64,16 @@ $settings = woo_get_dynamic_values( $settings );
 		
 		<!-- Twitter Section -->
 		<div class="twitter-section">
+			<!--
 			<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/god" data-widget-id="327062304757792768">Tweets by @god</a>
 			<br/>		
 			<a class="twitter-timeline" href="https://twitter.com/search?q=%40god" data-widget-id="327055876647174145">Tweets about "@god"</a>
-			
+			-->
 			<!-- twitter sdk js -->
 			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-			<!--
-			<a class="twitter-timeline" href="https://twitter.com/search?q=%40god" data-widget-id="327055876647174145">Tweets about "@god"</a>
-			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-			-->
-			<div id="data">
+			
+			<div id="data"></div>
 
-			</div>
 			<script type="text/javascript">
 				(function($){
 					$(function(){
