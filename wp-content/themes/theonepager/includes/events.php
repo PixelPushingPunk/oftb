12b606ecde67feddd72c13d3965b836e9e272430
Ar <hola.juapa@gmail.com>
2013-05-21 17:16:09 +0100

    Responsive ness and navigation

--- wp-content/themes/theonepager/includes/events.php
+++ wp-content/themes/theonepager/includes/events.php
@@ -113,6 +113,66 @@ $query = new WP_Query( array( 'page_id' => $settings['homepage_events_id'] ) );
 
 	<!-- End first Event Section -->
 
+
+
+	<!-- Third Event Section -->
+
+	<?php	if ( 0 < intval( $settings3['homepage_events3_id'] ) ) {
+		$query = new WP_Query( array( 'page_id' => $settings3['homepage_events3_id'] ) );
+	?>
+	<div id="events-section-three" class="col-full <?php echo esc_attr( $settings['homepage_posts_layout'] ); ?>">
+		<span class="heading"><?php echo $settings['homepage_events_posts_heading']; ?><!--Events--></span>
+		<div id="main" class="col-left">
+		<?php
+			if ( $query->have_posts() ) {
+				while ( $query->have_posts() ) { $query->the_post();
+		?>
+
+			<article <?php  if ( has_post_thumbnail() ) { echo 'class="has-featured-image"'; } ?>>
+
+				<?php if ( has_post_thumbnail() ) { ?>
+					<div class="featured-image">
+						<?php woo_image( 'width=500&noheight=true' ); ?>
+					</div>
+				<?php } ?>
+	
+				<div class="article-content article-events-two">
+
+					<header>
+						<h1><?php the_title(); ?></h1>
+					</header>
+
+					<section class="entry">
+						<?php the_content( __( 'Continue Reading &rarr;', 'woothemes' ) ); ?>
+					</section>
+
+				</div>
+
+			</article>
+
+			<div class="fix"></div>
+
+		<?php
+				} // End WHILE Loop
+				wp_reset_postdata();
+
+			} else {
+		?>
+		    <article <?php post_class(); ?>>
+		        <p><?php _e( 'Selected home page content not found.', 'woothemes' ); ?></p>
+		    </article><!-- /.post -->
+		<?php } ?>
+		</div><!--/#main .col-left-->
+		<?php
+			if ( $settings['homepage_posts_layout'] != 'layout-full' )  {
+				get_sidebar();
+			}
+		?>
+	</div>
+	<?php } // End the main check ?>
+	<!-- End Third Event Section -->
+
+
 	<!-- Second Event Section -->
 
 	<?php	if ( 0 < intval( $settings2['homepage_events2_id'] ) ) {
@@ -171,61 +231,5 @@ $query = new WP_Query( array( 'page_id' => $settings['homepage_events_id'] ) );
 	<?php } // End the main check ?>
 	<!-- End Second Event Section -->
 
-	<!-- Third Event Section -->
-
-	<?php	if ( 0 < intval( $settings3['homepage_events3_id'] ) ) {
-		$query = new WP_Query( array( 'page_id' => $settings3['homepage_events3_id'] ) );
-	?>
-	<div id="events-section-three" class="col-full <?php echo esc_attr( $settings['homepage_posts_layout'] ); ?>">
-		<span class="heading"><?php echo $settings['homepage_events_posts_heading']; ?><!--Events--></span>
-		<div id="main" class="col-left">
-		<?php
-			if ( $query->have_posts() ) {
-				while ( $query->have_posts() ) { $query->the_post();
-		?>
-
-			<article <?php  if ( has_post_thumbnail() ) { echo 'class="has-featured-image"'; } ?>>
-
-				<?php if ( has_post_thumbnail() ) { ?>
-					<div class="featured-image">
-						<?php woo_image( 'width=500&noheight=true' ); ?>
-					</div>
-				<?php } ?>
-	
-				<div class="article-content article-events-two">
-
-					<header>
-						<h1><?php the_title(); ?></h1>
-					</header>
-
-					<section class="entry">
-						<?php the_content( __( 'Continue Reading &rarr;', 'woothemes' ) ); ?>
-					</section>
-
-				</div>
-
-			</article>
-
-			<div class="fix"></div>
-
-		<?php
-				} // End WHILE Loop
-				wp_reset_postdata();
-
-			} else {
-		?>
-		    <article <?php post_class(); ?>>
-		        <p><?php _e( 'Selected home page content not found.', 'woothemes' ); ?></p>
-		    </article><!-- /.post -->
-		<?php } ?>
-		</div><!--/#main .col-left-->
-		<?php
-			if ( $settings['homepage_posts_layout'] != 'layout-full' )  {
-				get_sidebar();
-			}
-		?>
-	</div>
-	<?php } // End the main check ?>
-	<!-- End Third Event Section -->
 </div>
 
